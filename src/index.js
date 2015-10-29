1085aad00a80c9970750ca37e7f1caf6e77fe621
Tim Rodger <tim.rodger@gmail.com>
2015-10-29 09:32:05 +0000

    Adds empty put method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 var app = require('express')(),
     logger = require('./lib/logger');
 
+/**
+*  Set a template at path
+*/
+app.put('*', function(req, res) {
 
-app.put('/', function(req, res) {
+    logger.log('info', 'Adding a template at ' + req.path);
 
+    // use req.path as the identifier of the template
+
+    // respond with success
+    res.status(200);
 });
 
 var PORT = process.env.PORT || 80;
